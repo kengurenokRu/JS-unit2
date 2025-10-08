3afdcd0862f6f3b53b2bc6112772db3d5e1420b2
Asya Dunaeva <nastassiadunaeva@gmail.com>
2025-10-08 14:31:00 +0300

    fix: chenge list item

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
 'use strict';
 
-const itemOne = document.getElementsByClassName('props__item_one');
-const itemTwo = document.getElementsByClassName('props__item_two');
-const itemThree = document.getElementsByClassName('props__item_three');
-const itemFour = document.getElementsByClassName('props__item_four');
-const itemFive = document.getElementsByClassName('props__item_five');
-const itemSix = document.getElementsByClassName('props__item_six');
+
+
 
 const items = document.getElementsByClassName('items')[0].children;
-const itemTitle = document.getElementsByClassName('item__title');
 
 items[2].after(items[0]);
 items[3].after(items[2]);
 
+const itemTitle = document.getElementsByClassName('item__title');
 
 itemTitle[0].textContent = "Область видимости и замыкание";
 itemTitle[1].textContent = "This и прототипы объектов";
@@ -21,3 +17,16 @@ itemTitle[4].textContent = "ES6 и не только";
 
 const ads = document.getElementsByClassName('ads')[0];
 ads.remove();
+
+const propsItemTwo = document.getElementsByClassName('props__item_two');
+const propsItemFour = document.getElementsByClassName('props__item_four');
+
+propsItemFour[4].before(propsItemFour[0]);
+propsItemTwo[7].after(propsItemTwo[8]);
+propsItemTwo[8].after(propsItemTwo[9]);
+
+
+const propsList = document.getElementsByClassName('props__list');
+
+propsList[2].before(propsList[4]);
+itemTitle[3].after(propsList[3]);
