c6aae7c560f6771690e36f8e973bf0eee1c8c95f
kengrenokRu <nastassiadunaeva@gmail.com>
2025-10-13 12:37:17 +0300

    feat: add task les2

---
+++ js/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const title = document.getElementsByClassName('modal__title');
+const form = document.getElementsByClassName('modal__form');
+const checkbox = document.getElementsByClassName('modal__checkbox');
+const labelCheckbox = document.querySelector('.modal__checkbox-wrapper').previousElementSibling;
